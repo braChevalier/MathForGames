b92e5b0fb3a67332be24f2ac332884647fd98e40
braChevalier <braden.chevalier@gmail.com>
2019-09-21 13:55:35 -0400

    Added an exercise

--- GameMath/Assets/Resources/Scripts/Exercises/WhichSideOfTheLine.cs
+++ GameMath/Assets/Resources/Scripts/Exercises/WhichSideOfTheLine.cs
@@ -8,6 +8,7 @@ public class WhichSideOfTheLine : MonoBehaviour {
     Transform B;
     Transform C;
 
+    Renderer cRend;
     float sizeOfArea = 8;
     float moveSpeed = 3;
 
@@ -22,11 +23,21 @@ public class WhichSideOfTheLine : MonoBehaviour {
         LineRenderer rend = GetComponent<LineRenderer>();
         rend.SetPosition(0, 100 * (A.position - B.position) + A.position);
         rend.SetPosition(1, -100 * (A.position - B.position) + A.position);
-        C.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        cRend = C.gameObject.GetComponent<Renderer>();
+        cRend.material.color = Color.red;
     }
 	
 	
 	void Update () {
+        CheckForInput();
+        if(WhichSideOfLine(A.transform.position, B.transform.position, C.transform.position))
+            cRend.material.color = Color.red;
+        else
+            cRend.material.color = Color.green;
+    }
+
+    void CheckForInput()
+    {
         if (Input.GetKey(KeyCode.UpArrow))
             C.position += moveSpeed * Time.deltaTime * new Vector3(0, 1, 0);
         if (Input.GetKey(KeyCode.DownArrow))
@@ -35,8 +46,13 @@ public class WhichSideOfTheLine : MonoBehaviour {
             C.position += moveSpeed * Time.deltaTime * new Vector3(-1, 0, 0);
         if (Input.GetKey(KeyCode.RightArrow))
             C.position += moveSpeed * Time.deltaTime * new Vector3(1, 0, 0);
-
     }
 
+    //returns true if "pointToTest" lies on one side of the line formed by
+    //"v1" and "v2", and returns false if it lies on the other side
+    bool WhichSideOfLine(Vector2 v1, Vector2 v2, Vector2 pointToTest)
+    {
+        return false; //temp
+    }
     
 }
