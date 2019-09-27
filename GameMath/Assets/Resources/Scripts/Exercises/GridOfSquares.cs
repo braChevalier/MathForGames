4a32945b2d25c7547c9a6da70a6e70a4c2313f0c
braChevalier <braden.chevalier@gmail.com>
2019-09-27 17:34:45 -0400

    Grid exercise

---
+++ GameMath/Assets/Resources/Scripts/Exercises/GridOfSquares.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridOfSquares : MonoBehaviour {
+
+    GameObject squarePrefab;
+
+    int gridWidth = 10;
+    int gridHeight = 6;
+
+	void Start () {
+        squarePrefab = (GameObject)Resources.Load("Prefabs/Square");
+        SpawnSquares();
+	}
+	
+    //this function spawns a series of "squarePrefab" objects that are arranged in a "gridWidth" by "gridHeight" grid
+	void SpawnSquares()
+    {
+        
+    }
+}
