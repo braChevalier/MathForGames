8a4d318507041d56b28c82a495e3911216f56c37
braChevalier <braden.chevalier@gmail.com>
2019-10-04 15:22:30 -0400

    Physics exercises

---
+++ GameMath/Assets/Resources/Scripts/TriggerTarget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTarget : MonoBehaviour {
+
+    public Collider2D triggeringColi;
+
+	
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col == triggeringColi)
+            Debug.Log("You win!");
+    }
+	
+	
+}
